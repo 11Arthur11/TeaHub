2c87f53545193d2a75808fcba55563b41fb6af93
arthur <javadtoktamp@gmail.com>
2025-11-14 21:37:19 +0330

    Initial Commit

---
+++ src/main/java/dev/parhamziaei/teahub/TeaHubApplication.java
@@ -0,0 +1,11 @@
+package dev.parhamziaei.teahub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeaHubApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TeaHubApplication.class, args);
+    }
+}
---
+++ src/test/java/dev/parhamziaei/teahub/TeaHubApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.parhamziaei.teahub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeaHubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
